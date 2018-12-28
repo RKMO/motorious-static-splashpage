dc022f7e46d2fca2dca9ed5690a5fad18a426cd2
Katie Atwell <katieatwell@outlook.com>
2018-12-28 17:13:21 -0500

    Start animation for scroll snap sections

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,9 @@
-// This is where it all goes :)
+$(window).on('scroll', function() {
+  $('.bg-num').each(function(i, el) {
+    console.log(el, i)
+    var numPos = $(el).offset().top - $(window).scrollTop();
+    if (numPos <= 200) {
+      $(this).addClass('grow');
+    }
+  })
+})
