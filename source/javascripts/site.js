84bb98307dda0bdbc1c8bfce62d617745d602b06
Katie Atwell <katieatwell@outlook.com>
2019-01-02 17:30:22 -0500

    WIP

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,9 +1,63 @@
+//= require ./jquery.scrollify.js
+
+var scrollPos = 0;
+var preIndex = 0;
+var count = 0;
 $(window).on('scroll', function() {
+  var scrollT = $(window).scrollTop();
+  scrollPos = scrollT;
+})
+
+$(document).ready(function() {
+  $.scrollify({
+    section: ".section",
+    easing: "easeOutQuad",
+    scrollSpeed: 500,
+    scrollbars: false,
+    before: function(e) {
+      // preIndex = 0;
+    },
+    after: function(e) {
+      preIndex = parseInt($('body').attr('data-preIndex'));
+      if (e > preIndex) {
+        var prevEl = preIndex - 1;
+        $('.bg-num').each(function(i, el) {
+          if (i == preIndex) {
+            console.log('grow up')
+            $(this).addClass('grow').removeClass('shrink');
+          }  else if (i == prevEl) {
+            console.log('shrink up')
+            $(this).removeClass('grow').addClass('shrink');
+          }
+        })
+        count += 1;
+      } else {
+        var prevEl = preIndex + 1;
+        $('.bg-num').each(function(i, el) {
+          if (i == preIndex - 1) {
+            console.log('grow down')
+            $(this).removeClass('shrink').addClass('grow');
+            console.log($(this), i)
+          }
+          if (i == 0 && preIndex == 0) {
+            console.log('rm grow down')
+            $(this).removeClass('grow');
+          }
+        })
+      }
+      $('body').attr('data-preIndex', e);
+    },
+  });
+});
+
+function animateBGShrink(count) {
+  var prevEl = count - 1;
   $('.bg-num').each(function(i, el) {
-    console.log(el, i)
-    var numPos = $(el).offset().top - $(window).scrollTop();
-    if (numPos <= 200) {
-      $(this).addClass('grow');
+    if (i == prevEl) {
+      $(this).addClass('shrink').removeClass('grow');
     }
   })
-})
+}
+function animateBGGrow(count) {
+
+}
