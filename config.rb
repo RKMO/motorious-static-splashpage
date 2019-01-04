560d7ccfa942b1756a79bbdbafdbc4c210170a53
Katie Atwell <katieatwell@outlook.com>
2019-01-04 16:37:14 -0500

    Deploy

--- config.rb
+++ config.rb
@@ -1,38 +1,27 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
-
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'soon.motorious.com'
+  s3_sync.region                     = 'us-east-1'
+  s3_sync.aws_access_key_id          = ENV['SPEED_DIGITAL_AWS_ACCESS_KEY_ID']
+  s3_sync.aws_secret_access_key      = ENV['SPEED_DIGITAL_AWS_SECRET_ACCESS_KEY']
+  s3_sync.delete                     = false # do not delete stray files
+  s3_sync.after_build                = false # do not chain after the build step
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+end
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
+default_caching_policy max_age: (60 * 60 * 24 * 365), public: true
