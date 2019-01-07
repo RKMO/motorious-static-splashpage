ad5d4d12ceb0c9e66c5bcc52bdf21fadc2dc70c7
Katie Atwell <katieatwell@outlook.com>
2019-01-07 11:13:39 -0500

    SS

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
   $('form').on('submit', function(e) {
     event.preventDefault();
     __ss_noform.push(['submit', null, '2a3fd6a9-2bbb-4c66-a036-33b14f004ea5']);
-    this.submit()
+    this.submit();
     $('input[type=email]').val('');
     $('.expand .ta-center').text('Thank you for your interest!');
     $(this).slideToggle('slow');
