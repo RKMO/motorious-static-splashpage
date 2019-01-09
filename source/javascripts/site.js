a7f1d48dac6ee25b5fe0cd40822fc3a00fbea959
Katie Atwell <katieatwell@outlook.com>
2019-01-09 18:58:46 -0500

    Keep button on all pages

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -17,9 +17,7 @@ $(document).on('ready page:load page:fetch', function() {
     setHeights: false,
     afterRender:function() {
       $('body').attr('data-preIndex',0);
-      if (count == 0) {
-        $('.expand').addClass('active')
-      }
+      $('.expand').addClass('active');
     },
     before: function(e) {
       var direction,preIndex;
@@ -30,9 +28,6 @@ $(document).on('ready page:load page:fetch', function() {
           $("div[data-position='" + count + "']").addClass('shrink');
         }
         count += 1;
-        if (count == 1) {
-          $('.expand').removeClass('active');
-        }
       }else {
         count -= 1;
         $.scrollify.current().find('.bg-num').removeClass('shrink');
@@ -45,13 +40,7 @@ $(document).on('ready page:load page:fetch', function() {
       $.scrollify.current().find('.section-content').addClass('animate');
       $("div[data-position='" + (count - 1 ) + "']").next('.section-content').removeClass('animate');
       $("div[data-position='" + (count + 1 ) + "']").next('.section-content').removeClass('animate');
-      if (count == ($('.bg-num').length)) {
-        $('.expand').addClass('active');
-      } else if (count == 0) {
-        $('.expand').addClass('active');
-      } else {
-        $('.expand').removeClass('active');
-      }
+      $('expand').addClass('active');
     }
   })
   $('.prev').on('click', function(e) {
